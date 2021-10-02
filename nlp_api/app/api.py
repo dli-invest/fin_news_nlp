06802a89bf4908db23005d4f049a44bc5abca68a
David Li <davidli012345@gmail.com>
2021-10-02 18:28:20 +0000

    feat: github actions and updating cloudbuil

--- nlp_api/app/api.py
+++ nlp_api/app/api.py
@@ -54,7 +54,6 @@ async def extract_entities(body: RecordsRequest = Body(..., example=example_requ
         documents.append({"id": val.recordId, "text": val.data.text})
 
     entities_res = extractor.extract_entities(documents)
-
     res = [
         {"recordId": er["id"], "data": {"entities": er["entities"]}}
         for er in entities_res
--- nlp_api/app/tests/test_api.py
+++ nlp_api/app/tests/test_api.py
@@ -8,7 +8,7 @@ from app.api import app
 def test_docs_redirect():
     client = TestClient(app)
     response = client.get("/")
-    assert response.history[0].status_code == 302
+    assert response.history[0].status_code == 307
     assert response.status_code == 200
     assert response.url == "http://testserver/docs"
 
@@ -25,20 +25,23 @@ def test_api():
         "values": [{"recordId": "a1", "data": {"text": text, "language": "en"}}]
     }
 
-    response = client.post("/spacy_entities", json=request_data)
+    response = client.post("/entities", json=request_data)
     assert response.status_code == 200
 
     first_record = response.json()["values"][0]
     assert first_record["recordId"] == "a1"
     assert first_record["errors"] == None
     assert first_record["warnings"] == None
-
-    assert first_record["data"]["entities"] == [
+    # convert to flat list now
+    entities = first_record["data"]["entities"]
+    entities = [er_entity.get("name", None) for er_entity in entities]
+    # strip special characters and trim
+    clean_entities = [er.replace("\n", "").strip() for er in entities]
+    clean_entities.sort()
+    assert clean_entities == [
         "Alexa",
         "Amazon",
         "Apple",
-        "Echo and Dot",
-        "Google",
-        "iPhones",
-        "Siri",
+        "Echo",
+        "iPhones"
     ]
