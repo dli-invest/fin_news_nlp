4f2db455a21f98d10a7fa9d3c2062e26f9cbd5f5
David Li <davidli012345@gmail.com>
2021-10-02 18:04:08 +0000

    lint: remove unused code

--- nlp_api/main.py
+++ nlp_api/main.py
@@ -5,10 +5,4 @@ from app.api import app
 
 if __name__ == '__main__':
     port = os.getenv('PORT', 8080)
-    # try:
-    #     from waitress import serve
-    #     serve(app, host='0.0.0.0', port=port)
-    # except ImportError as e:
-    #     print(e)
-    #     app.run(host='0.0.0.0', debug=True)
     uvicorn.run(app, host='0.0.0.0', port=port, log_level='info')
