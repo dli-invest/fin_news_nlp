b6e2b8f2ecfa3d36358b2bfaf4bcec661d496bd1
David Li <davidli012345@gmail.com>
2021-10-06 15:18:21 +0000

    feat: basic url memory functionality added

--- scrappers/spiders/yahoo_finance.py
+++ scrappers/spiders/yahoo_finance.py
@@ -4,8 +4,10 @@ import dateparser
 import requests
 import json
 from datetime import datetime
-from scrapy.crawler import CrawlerProcess
+from scrapy import signals
 from bs4 import BeautifulSoup
+# Add https://stackoverflow.com/questions/12394184/scrapy-call-a-function-when-a-spider-quits
+# to next function
 # this doesn't work thanks seeking alpha, would need selenium
 from nlp_articles.app.nlp import init_nlp
 settings = {}
@@ -13,6 +15,7 @@ settings = {}
 nlp = init_nlp("https://raw.githubusercontent.com/dli-invest/fin_news_nlp/main/nlp_articles/core/data/exchanges.tsv", "https://raw.githubusercontent.com/dli-invest/fin_news_nlp/main/nlp_articles/core/data/indicies.tsv")
 
 
+output_file = "yahoo_urls.txt"
 class ScraperForYahoo(scrapy.Spider):
     name = "yahoo_finance"
     start_urls = [
@@ -26,8 +29,15 @@ class ScraperForYahoo(scrapy.Spider):
         "ca.finance.yahoo"
     ]
     read_article_urls = []
+    try:
+        with open(output_file) as file_in:
+            for line in file_in:
+                clean_line = line.replace("\n", "")
+                read_article_urls.append(clean_line)
+    except Exception as e:
+        print(e)
     rate = 2
-    webhook =  os.getenv('DISCORD_WEBHOOK')
+    webhook =  os.environ.get('DISCORD_WEBHOOK')
 
     def __init__(self):
         self.download_delay = 1/float(self.rate)
@@ -41,9 +51,12 @@ class ScraperForYahoo(scrapy.Spider):
                     continue
                 elif href[0:6] == "mailto":
                     continue
-                print(href)
                 href_merged = response.urljoin(href)
-                yield scrapy.Request(href_merged, callback=self.handle_article, dont_filter=True)
+                if href_merged not in self.read_article_urls:
+                    yield scrapy.Request(href_merged, callback=self.handle_article, dont_filter=True)
+                else:
+                    print("visited: " + href_merged)
+            # save list to file, append csv file
 
     def handle_article(self, response):
         url = response.url
@@ -102,10 +115,24 @@ class ScraperForYahoo(scrapy.Spider):
             self.post_webhook_content(data)
             self.read_article_urls.append(url)
 
-            print("_________________________________")
-            print(self.read_article_urls)
-            print("---------------------------------")
 
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(ScraperForYahoo, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_opened, signals.spider_opened)
+        crawler.signals.connect(spider.spider_closed, signals.spider_closed)
+        return spider
+
+    def spider_opened(self, spider):
+        print("spider opened")
+
+    def spider_closed(self, spider):
+        clean_list = list( dict.fromkeys(self.read_article_urls) )
+        if len(clean_list) > 0:
+            with open(output_file, 'w') as txt_file:
+                for article_url in clean_list:
+                    txt_file.write(article_url +"\n")
 
     def post_webhook_content(self, data: dict):
         url = self.webhook
