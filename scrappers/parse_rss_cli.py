7d02a1b694eca47e1c10f8d02af9a5726646a9aa
David Li <davidli012345@gmail.com>
2021-11-08 01:23:06 +0000

    feat: adding cnbc scrapping

---
+++ scrappers/parse_rss_cli.py
@@ -0,0 +1,64 @@
+# in main function get list of rss links and parse them
+import requests
+import json
+import os
+from scrappers.parse_rss_feeds import get_feed_data, parse_cnbc_feed, parse_the_guardian_feed
+
+
+def parse_output_file(output_file = "cnbc_urls.txt"):
+    """parse an output files and read the files"""
+
+    articles = []
+    try:
+        with open(output_file) as file_in:
+            for line in file_in:
+                clean_line = line.replace("\n", "")
+                articles.append(clean_line)
+    except Exception as e:
+        pass
+    return articles
+
+def save_list_of_strs_to_file(read_articles, file_name = "cnbc_urls.txt"):
+    """save list of strings to file"""
+
+    clean_list = list( dict.fromkeys(read_articles) )
+    if len(clean_list) > 0:
+        with open(file_name, 'w') as txt_file:
+            for article_url in clean_list:
+                txt_file.write(article_url +"\n")
+
+def post_webhook_content(data: dict):
+        url = os.environ.get('DISCORD_WEBHOOK')
+
+        result = requests.post(
+            url, data=json.dumps(data), headers={"Content-Type": "application/json"}
+        )
+
+        try:
+            result.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(err)
+        else:
+            print("Payload delivered successfully, code {}.".format(result.status_code))
+
+if __name__ == '__main__':
+
+    cnbc_feed_urls = ["https://www.cnbc.com/id/100003114/device/rss/rss.html"]
+    the_guardian_feed_urls = ["https://www.theguardian.com/environment/rss"]
+    stock_feed_list = [*cnbc_feed_urls, *the_guardian_feed_urls]
+
+    cnbc_output = "cnbc_urls.txt"
+    cnbc_read_articles = parse_output_file(cnbc_output)
+    # accumulate all the cnbc and the guardian feeds
+    for feed_url in stock_feed_list:
+        if feed_url.startswith("https://www.cnbc.com"):
+            if feed_url not in cnbc_read_articles:
+                cnbc_article = parse_cnbc_feed(feed_url)
+                cnbc_read_articles.append(cnbc_article)
+            # check if article is seen before
+        # eventually move the guardian article logic to the guardian api
+        elif feed_url.startswith("https://www.theguardian.com"):
+            guardian_article = parse_the_guardian_feed(feed_url)
+            continue
+
+    save_list_of_strs_to_file(cnbc_read_articles)
--- scrappers/parse_rss_feeds.py
+++ scrappers/parse_rss_feeds.py
@@ -1,1 +1,73 @@
 # use beautifulsoup to scrap the following cnbc rss feed https://www.cnbc.com/id/100003114/device/rss/rss.html
+from typing import List
+import requests
+from bs4 import BeautifulSoup
+
+def get_feed_data(request_url: str ='https://www.cnbc.com/id/100003114/device/rss/rss.html'):
+    # feeds the cnbc feeds for investing and uses beatifulsoup to parse the rss feed
+    feed = requests.get(request_url)
+    feed.raise_for_status()
+    feed_soup = BeautifulSoup(feed.text, "xml")
+    # gets the links for the cnbc feed
+    feed_items = feed_soup.findAll('item')
+
+    return feed_items
+
+def parse_cnbc_feed(feed_items: List[dict]):
+    """ grabs the data from the cnbc feed and parses it into a list of dictionaries
+    """
+    parsed_feed_data = []
+    for item in feed_items:
+        parsed_feed_data.append({
+            'title': item.title.text,
+            'link': item.link.text,
+            'description': item.description.text,
+            'pub_date': item.pubDate.text,
+            'guid': item.guid.text
+
+        })
+    return parsed_feed_data
+
+
+def rm_dups_from_list(list_of_dicts: List[dict]):
+    """ removes duplicate entries from a list of dictionaries
+    """
+    # create a set of all the dictionaries in the list
+    set_of_dicts = set(tuple(d.items()) for d in list_of_dicts)
+    # create a list of dictionaries from the set
+    list_of_dicts = [dict(d) for d in set_of_dicts]
+    return list_of_dicts    
+
+
+def parse_the_guardian_feed(feed_items: List[dict]):
+    """ grabs data from the guardian feed and parses it into a list of dictionaries
+    """
+    parsed_feed_data = []
+    for item in feed_items:
+        # get all categories for each feed item
+        categories_elements = item.findAll('category')
+
+        # get all unique categories from categories_elements
+        categories = []
+        categories = [{'category': category.text, "domain": category["domain"]} for category in categories_elements]
+        parsed_feed_data.append({
+            'title': item.title.text,
+            'link': item.link.text,
+            'description': item.description.text,
+            'pub_date': item.pubDate.text,
+            'guid': item.guid.text,
+            "categories": categories
+        })
+    return parsed_feed_data
+
+
+# links = get_feed_data()
+
+# data = parse_cnbc_feed(links)
+# # print(data)
+
+# guardian_data = get_feed_data('https://www.theguardian.com/environment/rss')
+
+# data = parse_the_guardian_feed(guardian_data)
+# print(data)
+# print(links)
