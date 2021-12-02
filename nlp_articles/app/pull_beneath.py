3dff1c485b846ba885200d3094446a7bfc49c842
David Li <davidli012345@gmail.com>
2021-12-02 05:41:38 +0000

    updating beneath logic

--- nlp_articles/app/pull_beneath.py
+++ nlp_articles/app/pull_beneath.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 import beneath
 import json
+from datetime import datetime, timedelta
 
 async def callback(record):
     print(record)
@@ -12,7 +13,11 @@ async def main():
     table = await client.find_table("examples/reddit/r-wallstreetbets-posts")
     table_instance = await table.find_instances()
     first_instance = table_instance[0]
-    filter = json.dumps({"created_on": { "_gte": "2021-03-09", "_lt": "2021-03-16"}})
+
+    start_date = datetime.today() - timedelta(hours=0, minutes=50)
+    start_time_str = start_date.strftime('%Y-%m-%dT%H:%M:%S')
+    end_time_str = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
+    filter = json.dumps({"created_on": { "_gte": start_time_str, "_lt": end_time_str}})
     cursor = await first_instance.query_index(filter=filter)
     records = await cursor.read_all()
     print(records)
