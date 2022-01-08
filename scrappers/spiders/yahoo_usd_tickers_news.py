f0ca4cee679127f48b1efe3ee387e63f2a8fd41b
David Li <davidli012345@gmail.com>
2022-01-08 22:24:35 +0000

    feat: updating requests

--- scrappers/spiders/yahoo_usd_tickers_news.py
+++ scrappers/spiders/yahoo_usd_tickers_news.py
@@ -240,5 +240,18 @@ class YahooUSDStockSpider(scrapy.Spider):
             result.raise_for_status()
         except requests.exceptions.HTTPError as err:
             print(err)
+            # convert data to raw bytes
+            # send raw bytes to discord as file
+            try:
+                requests.post(
+                    url,
+                    files={"file": ("file.json", json.dumps(data).encode("utf-8"))},
+                    headers={
+                        "Content-Type": "multipart/form-data",
+                    }
+                )
+            except requests.exceptions.HTTPError as err:
+                print(err)
+                pass
         else:
             print("Payload delivered successfully, code {}.".format(result.status_code))
