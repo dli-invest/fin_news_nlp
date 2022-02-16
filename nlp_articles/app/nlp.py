d9ccc0ae5a271678aea0c24746bb748648075ed6
David Li <davidli012345@gmail.com>
2022-02-15 19:41:53 -0800

    fix: closes #39 - switch news url and add more terms

--- nlp_articles/app/nlp.py
+++ nlp_articles/app/nlp.py
@@ -4,11 +4,11 @@ from nlp_articles.app.utils import stop_terms as stop_terms, DIVIDEND_LABEL
 # rewrite this to load based on patternl files.
 # https://spacy.io/usage/rule-based-matching#entityruler-files
 def init_nlp(exchange_data_path: str, indicies_data_path: str):
+    SPLIT_COMPANY_INTO_WORDS = True
     nlp = spacy.load("en_core_web_sm")
     ticker_df = pd.read_csv(
                 "https://raw.githubusercontent.com/dli-invest/eod_tickers/main/data/us.csv"
             )
-
     ticker_df = ticker_df.dropna(subset=['Code', 'Name'])
     ticker_df = ticker_df[~ticker_df.Name.str.contains("Wall Street", na=False)]
     # remove exact matches
@@ -50,21 +50,22 @@ def init_nlp(exchange_data_path: str, indicies_data_path: str):
     for company in companies:
         if company not in stops and len(company) > 1:
             patterns.append({"label": "COMPANY", "pattern": company})
-            words = company.split()
-            if len(words) >= 1:
-                new = " ".join(words)
-                if new not in first_words_added and new.isnumeric() == False:
-                    patterns.append({"label": "COMPANY", "pattern": new})
-                # add first word to list as well
-                first_word = words[0]
-                # ignore the numbers
-                if (
-                    first_word.isnumeric() == False
-                    and first_word.lower() not in stops
-                    and first_word not in first_words_added
-                ):
-                    first_words_added.append(first_word)
-                    patterns.append({"label": "COMPANY", "pattern": words[0]})
+            if SPLIT_COMPANY_INTO_WORDS:
+                words = company.split()
+                if len(words) >= 1:
+                    new = " ".join(words)
+                    if new not in first_words_added and new.isnumeric() == False:
+                        patterns.append({"label": "COMPANY", "pattern": new})
+                    # add first word to list as well
+                    first_word = words[0]
+                    # ignore the numbers
+                    if (
+                        first_word.isnumeric() == False
+                        and first_word.lower() not in stops
+                        and first_word not in first_words_added
+                    ):
+                        first_words_added.append(first_word)
+                        patterns.append({"label": "COMPANY", "pattern": words[0]})
 
     for index in indexes:
         patterns.append({"label": "INDEX", "pattern": index})
@@ -95,7 +96,7 @@ def init_nlp(exchange_data_path: str, indicies_data_path: str):
     for crit in ["acquisition", "buyout", "takeover"]:
          patterns.append({"label": "CRITICAL", "pattern": crit})
 
-    for term in ["COP", "BIDEN", "recession", "depression", "FED"]:
+    for term in ["COP", "BIDEN", "recession", "depression", "FED", "Quarter", "Earnings"]:
         patterns.append({"label": "THINGS", "pattern": term})
 
     for country in ["USA", "US", "United States", "U.S.", "U.S.A.", "CANADA", "CHINA"]:
@@ -117,6 +118,10 @@ def init_nlp(exchange_data_path: str, indicies_data_path: str):
         {
             "label": DIVIDEND_LABEL,
             "pattern":  [{"LOWER": "regular"}, {"LOWER": "dividend"}]
+        },
+        {
+            "label": DIVIDEND_LABEL,
+            "pattern": [{{"LOWER": "annual"}, {"LOWER": "dividend"}]
         }
     ]
     for pattern in DIVIDEND_PATTERNS:
