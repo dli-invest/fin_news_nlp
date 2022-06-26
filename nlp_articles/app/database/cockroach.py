1d1641613b53fd9f4d543b343c79a8564c711d64
David Li <davidli012345@gmail.com>
2022-06-26 15:09:45 +0000

    fix: updating cockroach file

---
+++ nlp_articles/app/database/cockroach.py
@@ -0,0 +1,21 @@
+# https://github.com/CodedotAl/gpt-code-clippy
+
+import psycopg2
+import sys
+def main():
+    if ((len(sys.argv)) !=2):
+        sys.exit("Error:No URL provided on command line")
+    uri=sys.argv[1]
+    connection = psycopg2.connect(uri)
+    cursor=connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS names")
+    cursor.execute("""CREATE TABLE names
+    (name String PRIMARY KEY NOT NULL)""")
+    cursor.execute("""INSERT INTO names (name)
+    VALUES('Ben'),('Jesse'),('Guy')""")
+    cursor.execute("SELECT name FROM names")
+    for row in cursor:
+        print(row[0])
+        cursor.close()
+        connection.close()
+main()
